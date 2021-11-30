574a70392d94a3ee02db0e7590c6536d848e8a82
Utkarsh Kumar <utkarshkanswal@gmail.com>
2021-11-30 17:40:53 +0530

    Updated

--- app.py
+++ app.py
@@ -9,12 +9,22 @@ from r2_score import r2_score
 from aic_bic import aic_bic_anaysis
 from actual_vs_predicted import actual_vs_predicted
 
-t = st.sidebar.radio(
+
+data_set=st.sidebar.radio('Choose Data Set ?',('OPC Sensor','PJ Sensor'))
+
+model_type=st.sidebar.radio('Choose Model Type ?',('Machine Learning Models','Neural Network Models'))
+
+if model_type=='Machine Learning Models':
+    t = st.sidebar.radio(
     'Choose One ?', ('Error Analysis', 'R2 Score', 'Actual vs Predicted Plot'))
 
-if t == "Error Analysis":
-    error_anaysis()
-elif t == "R2 Score":
-    r2_score()
-elif t == "Actual vs Predicted Plot":
-    actual_vs_predicted()
+    if t == "Error Analysis":
+        error_anaysis()
+    elif t == "R2 Score":
+        r2_score()
+    elif t == "Actual vs Predicted Plot":
+        actual_vs_predicted()
+elif model_type=='Neural Network Models':
+    pass        
+
+
